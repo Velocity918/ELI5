1b024dce47cf946a1deb39dcb7c80e43d0454933
Adhrit Gupta <powercreatorpoint@gmail.com>
2026-03-24 11:27:01 +0530

    flask addition

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 import backend
+from flask import Flask,render_template,request
 
-user_input = input()
+main = Flask(__name__)
 
-answer = backend.ELI5(user_input)
-print(answer)
+
+@main.route("/", methods = ["POST", "GET"])
+def home():
+    while True:
+        if request.method== 'POST':
+            query = request.form['query']
+            return backend.ELI5(query)
+        return render_template('index.html')
+    
+
+
+
+if __name__ == "__main__":
+    main.run(debug=True)
+# user_input = input()
+
+# answer = backend.ELI5(user_input)
+# print(answer)
