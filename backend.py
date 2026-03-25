a828cbf1c0d4859311130c97205be9e821dd11a9
Adhrit Gupta <powercreatorpoint@gmail.com>
2026-03-25 13:39:13 +0530

    flask update

--- backend.py
+++ backend.py
@@ -18,10 +18,7 @@ def ELI5(user_request):
     response = client.chat.send(
         model="openai/gpt-5-mini",
         messages=[
-            {"role": "system", "content": "You must follow all the rules:\n"
-                                        "1) You have explain what ever the user ask as if he is 5 years old\n"
-                                        "2) maximum word limit 150\n"
-                                        "3) Absolutely no Jargon"}, 
+            {"role": "system", "content": "Explain the user’s question as if they are 5 years old.\n\nRules:\n1. Use very simple, everyday words.\n2. Avoid jargon; if used, explain it simply.\n3. Assume the user has zero prior knowledge.\n4. Use short sentences (10–15 words max).\n5. Break the explanation into clear, logical steps.\n6. Use at least one simple real-life analogy.\n7. Include a basic example when possible.\n8. Focus on what the concept does, not abstract definitions.\n9. Avoid unnecessary detail or advanced theory.\n10. Stay strictly on topic and keep the explanation concise."}, 
             {"role": "user", "content": user_request}
         ],
     )
--- main.py
+++ main.py
@@ -1,23 +1,24 @@
 import backend
-from flask import Flask,render_template,request
+import flask
+from flask import Flask,render_template, request
 
 main = Flask(__name__)
 
 
 @main.route("/", methods = ["POST", "GET"])
 def home():
-    while True:
-        if request.method== 'POST':
-            query = request.form['query']
-            return backend.ELI5(query)
-        return render_template('index.html')
     
-
+    if request.method== 'POST':
+        query = request.form['query']
+        return render_template('answer.html', answer = backend.ELI5(query) )
+    return render_template("index.html")  
+    
+@main.route("/answer", methods = ["POST", "GET"])
+def answer():
+    if request.method == "POST":
+        return flask.redirect(flask.url_for("home"))
+    return render_template("answer.html")  
 
 
 if __name__ == "__main__":
     main.run(debug=True)
-# user_input = input()
-
-# answer = backend.ELI5(user_input)
-# print(answer)
